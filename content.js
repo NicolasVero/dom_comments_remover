05bd3b059e2d7b828bc88b105a6ef07060b69e4c
Nicolas Vero <123495341+NicolasVero@users.noreply.github.com>
2025-02-03 15:50:55 +0100

    Update content.js

--- content.js
+++ content.js
@@ -1,16 +1,35 @@
-(function removeComments(node) {
-	if (!node) {
-		return;
-	} 
+(function safeRemoveComments() {
+  function moveFocusIfNeeded(node) {
+    if (window.$0 === node) {
+      let newFocus = node.nextElementSibling || node.previousElementSibling;
+      if (newFocus) {
+        inspect(newFocus);
+      } else {
+        const placeholder = document.createTextNode('');
+        node.parentNode.insertBefore(placeholder, node.nextSibling);
+        inspect(placeholder);
+      }
+    }
+  }
 
-	if (node.nodeType === Node.COMMENT_NODE) {
-		node.remove();
-	} else {
-		let child = node.firstChild;
-		while (child) {
-			const next = child.nextSibling;
-			removeComments(child);
-			child = next;
-		}
-	}
-})(document);
+  function traverseAndRemove(node) {
+    if (!node) {
+		return;
+    }
+	  
+    let child = node.firstChild;
+    while (child) {
+      
+      const next = child.nextSibling;
+      if (child.nodeType === Node.COMMENT_NODE) {
+        moveFocusIfNeeded(child);
+        child.remove();
+      } else {
+        traverseAndRemove(child);
+      }
+      child = next;
+    }
+  }
+
+  traverseAndRemove(document);
+})();
